eba86699c5c27333c6582286a61be46c1bf098fc
Gui Constantino <gui.constantino@sigmagis.local>
2022-08-31 09:18:00 -0300

    Sincronização com travis

--- src/main/java/com/ambium/PrimeiroProjetoApplication.java
+++ src/main/java/com/ambium/PrimeiroProjetoApplication.java
@@ -8,7 +8,7 @@ public class PrimeiroProjetoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PrimeiroProjetoApplication.class, args);
-		System.out.println("Vai");
+		System.out.println("Vai q");
 	}
 
 }
