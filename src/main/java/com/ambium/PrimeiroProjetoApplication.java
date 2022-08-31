989b11c953ad9d1bfc2a5320dfda6104a86b34e1
Gui Constantino <gui.constantino@sigmagis.local>
2022-08-31 08:40:50 -0300

    Primeiro commit

--- src/main/java/com/ambium/PrimeiroProjetoApplication.java
+++ src/main/java/com/ambium/PrimeiroProjetoApplication.java
@@ -8,6 +8,7 @@ public class PrimeiroProjetoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PrimeiroProjetoApplication.class, args);
+		System.out.println("Vai");
 	}
 
 }
